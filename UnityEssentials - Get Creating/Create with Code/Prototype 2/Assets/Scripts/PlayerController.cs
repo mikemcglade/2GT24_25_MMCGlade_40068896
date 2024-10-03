c11beadd95b2d2105af82755b656e651a5f9b46d
mikemcglade <mcglade.michael@gmail.com>
2024-10-03 12:36:17 +0100

    Prototype 2 - added prefab pizza

--- UnityEssentials - Get Creating/Create with Code/Prototype 2/Assets/Scripts/PlayerController.cs
+++ UnityEssentials - Get Creating/Create with Code/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,17 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // keep the player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+         if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         
