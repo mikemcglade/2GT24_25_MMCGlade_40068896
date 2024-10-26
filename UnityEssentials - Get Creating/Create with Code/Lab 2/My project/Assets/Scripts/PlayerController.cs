d4636fdaa26da903d57ea3502d2febff4270b680
mikemcglade <mcglade.michael@gmail.com>
2024-10-26 12:27:15 +0100

    My Game - Constrained the player movement boundaries

--- UnityEssentials - Get Creating/Create with Code/Lab 2/My project/Assets/Scripts/PlayerController.cs
+++ UnityEssentials - Get Creating/Create with Code/Lab 2/My project/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private float speed = 10.0f;
+    private float zBound = 10;
     private Rigidbody playerRb;
 
     // Start is called before the first frame update
@@ -16,11 +17,29 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    
+    }
+
+    void MovePlayer()
+    {
         float horizonalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(Vector3.forward * speed * verticalInput);
         playerRb.AddForce(Vector3.right * speed * horizonalInput);
+    }
 
+    void ConstrainPlayerPosition()
+    {
+    if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
     }
 }
