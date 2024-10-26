6e403e58a4b5be1c3c6dafe3b484766da313ebc1
mikemcglade <mcglade.michael@gmail.com>
2024-10-26 14:36:53 +0100

    My Game - spawn manager random applied

--- UnityEssentials - Get Creating/Create with Code/Lab 2/My project/Assets/Scripts/SpawnManager.cs
+++ UnityEssentials - Get Creating/Create with Code/Lab 2/My project/Assets/Scripts/SpawnManager.cs
@@ -5,23 +5,26 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] enemyPrefabs; 
-    private float spawnRangeX = 17;
+    private float spawnRangeX = 16;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("spawnRandomEnemy", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)){
-            // randomly generate enemy ndex and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+    
+    }
+    void spawnRandomEnemy(){
+    Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             int enemyIndex = Random.Range(0, enemyPrefabs.Length);
             Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
-        }
+
     }
 }
