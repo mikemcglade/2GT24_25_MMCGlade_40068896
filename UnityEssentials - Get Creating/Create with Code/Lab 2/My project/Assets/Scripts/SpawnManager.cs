a19aad61c1f41bea696d6bf14490985bcbdfb46e
mikemcglade <mcglade.michael@gmail.com>
2024-10-26 14:26:59 +0100

    My Game - spawn manager script applied

--- UnityEssentials - Get Creating/Create with Code/Lab 2/My project/Assets/Scripts/SpawnManager.cs
+++ UnityEssentials - Get Creating/Create with Code/Lab 2/My project/Assets/Scripts/SpawnManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] enemyPrefabs; 
+    private float spawnRangeX = 17;
+    private float spawnPosZ = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,11 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S)){
+            // randomly generate enemy ndex and spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int enemyIndex = Random.Range(0, enemyPrefabs.Length);
+            Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
+        }
     }
 }
