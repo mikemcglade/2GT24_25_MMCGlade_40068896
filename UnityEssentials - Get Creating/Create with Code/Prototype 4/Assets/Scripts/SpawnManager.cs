80603151edaabd72396f8fdda7d154a3f8a67f6b
mikemcglade <mcglade.michael@gmail.com>
2024-11-01 15:48:39 +0000

    Prototype 4 - Spawn Powerups with new waves

--- UnityEssentials - Get Creating/Create with Code/Prototype 4/Assets/Scripts/SpawnManager.cs
+++ UnityEssentials - Get Creating/Create with Code/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9.0f;
     public int enemyCount;
     public int waveNumber = 1;
@@ -13,6 +14,7 @@ public class SpawnManager : MonoBehaviour
     void Start()
     {
         SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
@@ -24,6 +26,7 @@ public class SpawnManager : MonoBehaviour
         {
             waveNumber++;
             SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
         }
     }
 
@@ -31,7 +34,7 @@ public class SpawnManager : MonoBehaviour
     {
         for (int i=0; i < enemiesToSpawn; i++)
         {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
         }
     }
 
