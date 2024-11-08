0ffd9e3f8ce24c6a61700783612d13111301f6be
mikemcglade <mcglade.michael@gmail.com>
2024-11-08 17:28:59 +0000

    Prototype 5 - Add a Particle explosion

--- UnityEssentials - Get Creating/Create with Code/Prototype 5/Assets/Scripts/Target.cs
+++ UnityEssentials - Get Creating/Create with Code/Prototype 5/Assets/Scripts/Target.cs
@@ -12,6 +12,10 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
+   
+    public ParticleSystem explosionParticle;
+   
+    public int pointValue;
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +33,8 @@ public class Target : MonoBehaviour
         private void OnMouseDown()
     {
         Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
 
     private void OnTriggerEnter(Collider other)
