6151227609b8d0416e053ea4011fdadc8f17c932
mikemcglade <mcglade.michael@gmail.com>
2024-11-09 12:11:20 +0000

    Prototype 5 - buttons start the game

--- UnityEssentials - Get Creating/Create with Code/Prototype 5/Assets/Scripts/DifficultyButton.cs
+++ UnityEssentials - Get Creating/Create with Code/Prototype 5/Assets/Scripts/DifficultyButton.cs
@@ -6,16 +6,25 @@ using UnityEngine.UI;
 public class DifficultyButton : MonoBehaviour
 {
     private Button button;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + " was clicked");
+        gameManager.StartGame();
     }
 }
