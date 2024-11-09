d95be6749045488bf70b1c2a3dd49bf288e9dc77
mikemcglade <mcglade.michael@gmail.com>
2024-11-09 12:28:08 +0000

    Prototype 5 - Use a parameter to change difficulty

--- UnityEssentials - Get Creating/Create with Code/Prototype 5/Assets/Scripts/DifficultyButton.cs
+++ UnityEssentials - Get Creating/Create with Code/Prototype 5/Assets/Scripts/DifficultyButton.cs
@@ -7,6 +7,7 @@ public class DifficultyButton : MonoBehaviour
 {
     private Button button;
     private GameManager gameManager;
+    public int difficulty;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +26,6 @@ public class DifficultyButton : MonoBehaviour
     void SetDifficulty()
     {
         Debug.Log(button.gameObject.name + " was clicked");
-        gameManager.StartGame();
+        gameManager.StartGame(difficulty);
     }
 }
