0f11b518eef73a86afe0457b0032e39a105d1de2
mikemcglade <mcglade.michael@gmail.com>
2024-11-17 16:33:54 +0000

    my games - added music to game start screen - works

--- UnityEssentials - Get Creating/Create with Code/Lab 2/My project/Assets/Scripts/StartScreenManager.cs
+++ UnityEssentials - Get Creating/Create with Code/Lab 2/My project/Assets/Scripts/StartScreenManager.cs
@@ -1,8 +1,34 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartScreenManager : MonoBehaviour
 {
+
+    public AudioSource backgroundMusic;
+    //public Animation startScreenAnimation;
+    //public Button startButton;
+
+    void Start()
+    {
+        // Play background music
+        if (backgroundMusic != null)
+        {
+            backgroundMusic.Play();
+        }
+
+        // Play start screen animation
+        // if (startScreenAnimation != null)
+        // {
+        //     startScreenAnimation.Play();
+        // }
+
+        // Add listener to start button
+        //if (startButton != null)
+        //{
+        //    startButton.onClick.AddListener(StartGame);
+        //}
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("Level 1");
