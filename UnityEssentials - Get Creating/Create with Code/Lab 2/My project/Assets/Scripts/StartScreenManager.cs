ebd6594c6736754b71ce23a110d01a2967538dce
mikemcglade <mcglade.michael@gmail.com>
2024-11-26 11:37:56 +0000

    my game - added game play instructions to start screen

--- UnityEssentials - Get Creating/Create with Code/Lab 2/My project/Assets/Scripts/StartScreenManager.cs
+++ UnityEssentials - Get Creating/Create with Code/Lab 2/My project/Assets/Scripts/StartScreenManager.cs
@@ -6,8 +6,13 @@ public class StartScreenManager : MonoBehaviour
 {
 
     public AudioSource backgroundMusic;
+    public GameObject instructionsPanel;
+    public Button instructionsButton;
+    public Button startButton;
+    public Button closeButton; // Add this line
+
     //public Animation startScreenAnimation;
-    //public Button startButton;
+
 
     void Start()
     {
@@ -24,13 +29,46 @@ public class StartScreenManager : MonoBehaviour
         // }
 
         // Add listener to start button
-        //if (startButton != null)
-        //{
-        //    startButton.onClick.AddListener(StartGame);
-        //}
+        if (startButton != null)
+        {
+           startButton.onClick.AddListener(StartGame);
+        }
+        
+        if (instructionsButton != null)
+        {
+            instructionsButton.onClick.AddListener(ShowInstructions);
+        }
+
+        // Add listener to close button
+        if (closeButton != null)
+        {
+            closeButton.onClick.AddListener(HideInstructions);
+        }
+
+        // Hide instructions panel initially
+        if (instructionsPanel != null)
+        {
+            instructionsPanel.SetActive(false);
+        }
     }
     public void StartGame()
     {
         SceneManager.LoadScene("Level 1");
+    }
+
+    public void ShowInstructions()
+    {
+        if (instructionsPanel != null)
+        {
+            instructionsPanel.SetActive(true);
+        }
+    }
+
+    public void HideInstructions()
+    {
+        if (instructionsPanel != null)
+        {
+            instructionsPanel.SetActive(false);
+        }
     }
 }
