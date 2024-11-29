15b5c3ea53e966e4793b9cd9836707677f2ab8e4
mikemcglade <mcglade.michael@gmail.com>
2024-11-29 16:54:49 +0000

    Prototype 1 - added serialize field to follow player

---
+++ UnityEssentials - Get Creating/Create with Code/My Story - Hellboy/Assets/Scripts/CogMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class CogMovement : MonoBehaviour
+{
+    public float rotationSpeed = 30f;
+    public float judderIntensity = 0.5f;
+    public float judderFrequency = 2f;
+
+    private Quaternion initialRotation;
+    private float time;
+
+    void Start()
+    {
+        initialRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        // Calculate smooth rotation
+        float smoothRotation = rotationSpeed * time;
+
+        // Calculate judder effect
+        float judder = Mathf.Sin(time * judderFrequency * Mathf.PI) * judderIntensity;
+
+        // Combine smooth rotation and judder
+        float finalRotation = smoothRotation + judder;
+
+        // Apply rotation to the cog while preserving initial orientation
+        transform.rotation = initialRotation * Quaternion.Euler(0f, 0f, finalRotation);
+    }
+}
---
+++ UnityEssentials - Get Creating/Create with Code/My Story - Hellboy/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Animator anim;
+    private Rigidbody rb;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Jump();
+        Move();
+        still();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rb.AddForce(Vector3.up * 10 * Time.deltaTime, ForceMode.Impulse);
+            this.anim.SetBool("jump", true);
+        }
+        else
+        {
+            this.anim.SetBool("jump", false);
+        }
+    }
+
+    private void Move()
+    {
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxis = Input.GetAxis("Horizontal");
+
+        Vector3 movement = this.transform.forward * verticalAxis + this.transform.right * horizontalAxis;
+        movement.Normalize();
+
+        this.transform.position += movement * 0.02f;
+
+        this.anim.SetFloat("vertical", verticalAxis);
+        this.anim.SetFloat("horizontal", horizontalAxis);
+    }
+
+    private void still()
+    {
+        this.rb.AddForce(Vector3.down * 1 * Time.deltaTime, ForceMode.Impulse);
+    }
+}
