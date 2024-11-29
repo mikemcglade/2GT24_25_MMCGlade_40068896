b426902a829f0b9c17688a8fe5448c034ccefb34
mikemcglade <mcglade.michael@gmail.com>
2024-11-29 17:04:29 +0000

    Prototype 1 - Unity Event Functions exploration

--- UnityEssentials - Get Creating/Create with Code/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ UnityEssentials - Get Creating/Create with Code/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -6,17 +6,10 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     [SerializeField] private Vector3 offset = new Vector3(0, 5, -7);
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void LateUpdate()
     {
         // offset the camera behind the player by adding to the player's position
         transform.position = player.transform.position + offset;
-
     }
 }
--- UnityEssentials - Get Creating/Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ UnityEssentials - Get Creating/Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -10,14 +10,7 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float forwardInput;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // player inputs
         horizontalInput = Input.GetAxis("Horizontal");
